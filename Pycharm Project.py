5880e1e77bd419c765c4c17c665e6ebec7e837e3
aniger429 <regina_balajadia@dlsu.edu.ph>
2017-02-04 18:05:25 +0800

    initial

---
+++ Pycharm Project.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("base.html")
+@app.route('/datacleaning')
+def datacleaning():
+    return render_template("datacleaning.html")
+
+
+if __name__ == '__main__':
+    app.run()
